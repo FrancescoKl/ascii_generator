f37fe6a0bcab68c39c04590e8cc6e2fe10a10961
Daniele DM <daniele.dm@gmail.com>
2018-06-07 19:31:41 +0200

    reverse string and edge detection

--- asciigen.py
+++ asciigen.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from PIL import Image
+from PIL import Image, ImageFilter
 
 from exception import ImageTooSmall
 
@@ -8,10 +8,10 @@ from exception import ImageTooSmall
 # http://paulbourke.net/dataformats/asciiart/
 
 # 70 levels of gray
-gscale1 = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+gscale1 = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'._" # changed space with underscore
 
 # 10 levels of gray
-gscale2 = '@%#*+=-:. '
+gscale2 = '@%#*+=-:._' # changed space with underscore
 
 
 def getAverageL(image):
@@ -28,7 +28,21 @@ def getAverageL(image):
     return np.average(im.reshape(w * h))
 
 
-def covertImageToAscii(fileName, cols, scale, moreLevels):
+def getMaxL(image):
+    """
+    Given PIL Image, return average value of grayscale value
+    """
+    # get image as numpy array
+    im = np.array(image)
+
+    # get shape
+    w, h = im.shape
+
+    # get average
+    return np.max(im.reshape(w * h))
+
+
+def covertImageToAscii(fileName, cols, scale, moreLevels, edge = False):
     """
     Given Image and dims (rows, cols) returns an m*n list of Images
     """
@@ -37,6 +51,11 @@ def covertImageToAscii(fileName, cols, scale, moreLevels):
 
     # open image and convert to grayscale
     image = Image.open(fileName).convert('L')
+    if edge:
+        image = image.filter(ImageFilter.FIND_EDGES)
+        gscale1 = gscale1[::-1] # reverse, darker background become a "lighter" char
+        gscale2 = gscale2[::-1]
+        image.save("/tmp/edge.jpg")
 
     # store dimensions
     W, H = image.size[0], image.size[1]
@@ -81,8 +100,12 @@ def covertImageToAscii(fileName, cols, scale, moreLevels):
             # crop image to extract tile
             img = image.crop((x1, y1, x2, y2))
 
-            # get average luminance
-            avg = int(getAverageL(img))
+            if edge:
+                # get average luminance
+                avg = int(getMaxL(img))
+            else:
+                # get average luminance
+                avg = int(getAverageL(img))
 
             # look up ascii char
             if moreLevels:
@@ -99,4 +122,3 @@ def covertImageToAscii(fileName, cols, scale, moreLevels):
 def convert_image(imgFile, scale=0.43, cols=80, moreLevels=False):
     aimg = covertImageToAscii(imgFile, cols, scale, moreLevels)
     return aimg
-
