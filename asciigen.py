563032cfabc3b6c03961deb9abcea5177a3b4958
Francesco Wood <francesco.wood@immobiliare.it>
2018-06-08 17:41:07 +0200

    minor refactoring

--- asciigen.py
+++ asciigen.py
@@ -42,7 +42,7 @@ def getMaxL(image):
     return np.max(im.reshape(w * h))
 
 
-def covertImageToAscii(fileName, cols, scale, moreLevels, edge = False):
+def covertImageToAscii(fileName, cols, scale, moreLevels, edge):
     """
     Given Image and dims (rows, cols) returns an m*n list of Images
     """
@@ -55,7 +55,7 @@ def covertImageToAscii(fileName, cols, scale, moreLevels, edge = False):
         image = image.filter(ImageFilter.FIND_EDGES)
         gscale1 = gscale1[::-1] # reverse, darker background become a "lighter" char
         gscale2 = gscale2[::-1]
-        image.save("/tmp/edge.jpg")
+        #image.save("/tmp/edge.jpg")
 
     # store dimensions
     W, H = image.size[0], image.size[1]
@@ -78,11 +78,7 @@ def covertImageToAscii(fileName, cols, scale, moreLevels, edge = False):
     # generate list of dimensions
     for j in range(rows):
         y1 = int(j * h)
-        y2 = int((j + 1) * h)
-
-        # correct last tile
-        if j == rows - 1:
-            y2 = H
+        y2 = int((j + 1) * h) if j != rows - 1 else H
 
         # append an empty string
         aimg.append("")
@@ -91,27 +87,14 @@ def covertImageToAscii(fileName, cols, scale, moreLevels, edge = False):
 
             # crop image to tile
             x1 = int(i * w)
-            x2 = int((i + 1) * w)
-
-            # correct last tile
-            if i == cols - 1:
-                x2 = W
+            x2 = int((i + 1) * w) if i != cols - 1 else W
 
             # crop image to extract tile
             img = image.crop((x1, y1, x2, y2))
 
-            if edge:
-                # get average luminance
-                avg = int(getMaxL(img))
-            else:
-                # get average luminance
-                avg = int(getAverageL(img))
+            avg = int(getMaxL(img)) if edge else int(getAverageL(img))
 
-            # look up ascii char
-            if moreLevels:
-                gsval = gscale1[int((avg * 69) / 255)]
-            else:
-                gsval = gscale2[int((avg * 9) / 255)]
+            gsval = gscale1[int((avg * 69) / 255)] if moreLevels else gscale2[int((avg * 9) / 255)]
 
             # append ascii char to string
             aimg[j] += gsval
@@ -119,6 +102,7 @@ def covertImageToAscii(fileName, cols, scale, moreLevels, edge = False):
     # return txt image
     return aimg
 
-def convert_image(imgFile, scale=0.43, cols=80, moreLevels=False):
-    aimg = covertImageToAscii(imgFile, cols, scale, moreLevels)
+
+def convert_image(imgFile, scale=0.43, cols=80, moreLevels=False, edge=False):
+    aimg = covertImageToAscii(imgFile, cols, scale, moreLevels, edge)
     return aimg
